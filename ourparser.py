aa6289177eb543f759ea5bdc38051af8f23c149a
bichengliu <bicheng_liu@hotmail.com>
2016-03-05 00:55:37 -0500

    pseudo-code updated

---
+++ ourparser.py
@@ -0,0 +1,30 @@
+# read file here
+
+
+# assume first two lines are gone (Jeremie please get on this for the website)
+
+
+# while (line != eof)
+# if line elements 5 -> 8 are a space, and 6, 7, 8 are all numbers,
+# create a .ics file named line (go to next line)
+
+
+# read course code (call it current)
+
+
+
+# if the course codes (current and prev) are the same, do NOTHING
+# else, close current .ics file, and create a new .ics file
+
+
+# read line and make note of the first 3 characters in the line
+#     while(not ACADEMIC...)
+#        save all information to the respective arrays
+#        type_dates (i) = read line (assume it to be dates) go to next line
+#        type_places (i) = read line (assume it to be place) go to next line
+#        i = i + 1
+#     end while
+# save the course code for now (call it prev)
+# end if
+
+#end while
